04b1cfdb248f53d2f07bea850fecaad158faf103
lucas diaz <lucasdiaz687@gmail.com>
2021-12-02 17:41:50 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'holait';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = '7';
+const nuevoNum = '8';
 
 // Crea una variable booleana:
 const nuevoBool = null;
